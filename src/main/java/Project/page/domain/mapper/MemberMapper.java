77df2d33f0ac9456d47e5e1b468fc7f72e7a6f4f
문광희 <mgh7020@gmail.com>
2023-01-15 20:00:31 +0900

    Revert "merge - 2023-01-15 homework"
    This reverts commit 25347fb54b1a1e830d2dddfc5651582de238e5c5.

--- src/main/java/Project/page/domain/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package Project.page.domain.mapper;
-
-import Project.page.Users.UserVo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberMapper {
-    void saveUser(UserVo userVo);
-}
